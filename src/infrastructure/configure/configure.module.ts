5e5769ee54e138c4e2e97760e2f99423e7cfdaf1
Paulo Dutra <paulodutra.info@gmail.com>
2025-06-10 17:12:19 -0300

    chore: setup

---
+++ src/infrastructure/configure/configure.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+  ],
+})
+export class ConfigureModule {}
